ccf9d8e85a6f4d3c4a9027c62df25074d0ad1a5c
happytrollfriend <73976750+happytrollfriend@users.noreply.github.com>
2020-11-10 00:29:24 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,124 +1,150 @@
 const input = document.getElementById("input");
 const output = document.getElementById("output");
-const sign = document.getElementById("sign");
-
-const sum = document.getElementById("sumBtn");
-const sub = document.getElementById("subBtn");
-const muti = document.getElementById("mutiBtn");
-const div = document.getElementById("divisionBtn");
+const signOutput = document.getElementById("sign");
 
 const submit = document.getElementById("equalBtn");
 
-let inputValue = [];
-let tempValueOne = "";
+var inputValue = {
+  inputOne: "",
+  inputTwo: "",
+  enableInput: "inputOne",
+  initialize: false,
+  isToggle: false,
+  method: "",
+  sign: "",
+  process: function (a) {
+    if (this.enableInput == "inputOne" && this.isToggle == false) {
+      this.inputOne += a;
+    } else {
+      this.inputTwo += a;
+    }
+  },
+};
 
-let result = 0;
-let operation = "";
+const inputVal = inputValue;
 
-sum.addEventListener("click", () => {
-  operation = "+";
-  operator();
-  displayIO();
+var eventListener = document.addEventListener("click", (r) => {
+  const inputVal = inputValue;
+  const path = r.path[0].id;
+  const number = [
+    "zeroBtn",
+    "oneBtn",
+    "twoBtn",
+    "threeBtn",
+    "fourBtn",
+    "fiveBtn",
+    "sixBtn",
+    "sevenBtn",
+    "eightBtn",
+    "nineBtn",
+  ];
+  const signBtn = ["sumBtn", "subBtn", "mutiBtn", "divisionBtn", "equalBtn"];
+
+  number.forEach((a, b) => {
+    if (path == a) {
+      inputVal.process(b.toString());
+      console.table({
+        inputVal,
+      });
+    }
+  });
+  signBtn.forEach((a) => {
+    if (path == a) {
+      calMethod(a, inputVal);
+    }
+  });
+
+  output.innerText = inputVal.inputOne;
+  input.innerText = inputVal.inputTwo;
+  signOutput.innerText = inputVal.sign;
 });
 
-sub.addEventListener("click", () => {
-  operation = "-";
-  operator();
-  displayIO();
-});
+function calMethod(a, b) {
+  if (b.isToggle == false || b.initialize != true) {
+    switch (a) {
+      case "sumBtn":
+        b.method = "add";
+        b.sign = "+";
+        break;
+      case "subBtn":
+        b.method = "subtract";
+        b.sign = "-";
+        break;
+      case "mutiBtn":
+        b.method = "multiply";
+        b.sign = "*";
+        break;
+      case "divisionBtn":
+        b.method = "divide";
+        b.sign = "/";
+        break;
 
-muti.addEventListener("click", () => {
-  operation = "*";
-  operator();
-  displayIO();
-});
-
-div.addEventListener("click", () => {
-  operation = "/";
-  operator();
-  displayIO();
-});
-
-submit.addEventListener("click", () => {
-  operator();
-  operation = "";
-  displayIO();
-});
-
-function enterNumber(val) {
-  if (inputValue.length == 0 && val == 0) {
+      default: {
+        console.log("none");
+      }
+    }
+    b.isToggle = true;
+    b.enableInput = "inputTwo";
+    b.initialize = true;
   } else {
-    inputValue.push(val);
-    displayIO();
+    if (a == "equalBtn" && b.inputTwo != "") {
+      b.isToggle = false;
+      switch (b.method) {
+        case "add":
+          b.inputOne = calculate(
+            parseInt(b.inputOne),
+            parseInt(b.inputTwo)
+          ).add;
+          break;
+        case "subtract":
+          b.inputOne = calculate(
+            parseInt(b.inputOne),
+            parseInt(b.inputTwo)
+          ).subtract;
+          break;
+        case "multiply":
+          b.inputOne = calculate(
+            parseInt(b.inputOne),
+            parseInt(b.inputTwo)
+          ).multiply;
+          break;
+        case "divide":
+          b.inputOne = calculate(
+            parseInt(b.inputOne),
+            parseInt(b.inputTwo)
+          ).divide;
+          break;
+
+        default: {
+          console.log("none");
+        }
+      }
+      b.inputTwo = "";
+      b.method = "";
+      b.sign = "";
+    } else {
+      console.log("not");
+    }
   }
 }
 
-function operator() {
-  if (inputValue.length == 0) {
-    console.log("nothing to cal");
-  } else {
-    inputValue.forEach((r) => {
-      tempValueOne += r;
-    });
-    tempValueOne = parseInt(tempValueOne);
-    if (result == 0) {
-      result = tempValueOne;
-    } else {
-      tempValueOne = parseInt(tempValueOne);
-
-      switch (operation) {
-        case "+": {
-          result += tempValueOne;
-
-          break;
-        }
-        case "-": {
-          result -= tempValueOne;
-
-          break;
-        }
-        case "*": {
-          result *= tempValueOne;
-
-          break;
-        }
-        case "/": {
-          result /= tempValueOne;
-
-          break;
-        }
-        default: {
-          console.log(result);
-          break;
-        }
-      }
-    }
-    inputValue = [];
-    tempValueOne = "";
-
-    displayIO();
-  }
+function calculate(a, b) {
+  return {
+    inputOne: a,
+    inputTwo: b,
+    add: a + b,
+    subtract: a - b,
+    multiply: a * b,
+    divide: a / b,
+  };
 }
 
 function clearAll() {
-  inputValue = [];
-  tempValueOne = "";
-
-  result = 0;
-  operation = "";
-  displayIO();
-}
-
-function clearOneDigi() {}
-
-function displayIO() {
-  if (inputValue.length == 0) {
-    input.innerText = inputValue;
-    output.innerText = result;
-    sign.innerText = operation;
-  } else {
-    input.innerText += inputValue[inputValue.length - 1];
-    output.innerText = result;
-    sign.innerText = operation;
-  }
+  inputVal.inputOne = "";
+  inputVal.inputTwo = "";
+  inputVal.enableInput = "inputOne";
+  inputVal.isToggle = false;
+  inputVal.initialize = false;
+  inputVal.method = "";
+  inputVal.sign = "";
 }
